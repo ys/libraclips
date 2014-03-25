a59829d93ef25c7e4943ccb7e728a9b6615740f3
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-25 15:52:12 +0100

    add Transformer box to transform data from dataclips to librato Array of metrics

---
+++ lib/transformer.rb
@@ -0,0 +1,27 @@
+require 'scrolls'
+require_relative 'dataclips'
+require_relative 'transform_functions'
+
+class Transformer
+  def initialize(dataclip_ref, librato_base_name = nil)
+    @dataclip_ref = dataclip_ref
+    @librato_base_name = librato_base_name
+  end
+
+  def call
+    return if dataclip.empty?
+    transform_function.call(dataclip, librato_base_name: @librato_base_name)
+  end
+
+  def transform_function
+    @transform_function ||= TransformFunctions.find_for(dataclip)
+  end
+
+  def dataclip
+    @dataclip ||= dataclips_client.fetch(@dataclip_ref)
+  end
+
+  def dataclips_client
+    @dataclips_client ||= Dataclips::Client.new
+  end
+end
