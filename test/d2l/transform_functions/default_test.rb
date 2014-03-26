6fe53cadbea147392640a3219e8a07a8650b4369
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-26 16:41:53 +0100

    Add test for Default transform

---
+++ test/d2l/transform_functions/default_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class D2L::TransformFunctions::DefaultTest < Minitest::Unit::TestCase
+  def valid_dataclip
+    fields = ['name','count', 'perc100']
+    values = (0..10).map do |i|
+      Struct.new(*fields.map(&:to_sym)).new("name#{i}", i * 10, i * 100)
+    end
+    D2L::Dataclips::Result.new(fields, values)
+  end
+
+  def valid_metrics
+    metrics = {}
+    (0..10).each do |i|
+        metrics["default.name#{i}.count"]   = { value: i * 10 }
+        metrics["default.name#{i}.perc100"] = { value: i * 100 }
+    end
+    { gauges: metrics }
+  end
+
+  def test_valid_dataclip_is_accepted
+    assert D2L::TransformFunctions::Default.new.accepts?(valid_dataclip)
+  end
+
+  def test_valid_dataclip_is_correctly_transformed
+    assert_equal D2L::TransformFunctions::Default.new.call(valid_dataclip), valid_metrics
+  end
+end
