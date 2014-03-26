3efd9a4a6e7d9127adffa042e5c9af28591d2b9e
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-26 16:15:11 +0100

    Simplify base transform function.
    Support a column containing name as the metric name
    And reports columns count, perc** as different metrics to librato

--- lib/d2l/transform_functions/base.rb
+++ lib/d2l/transform_functions/base.rb
@@ -1,40 +1,41 @@
+require_relative '../config'
+
 module D2L
   module TransformFunctions
     class Base
       def accepts?(dataclip)
-        dataclip.has_field?('count')
+        dataclip.has_field?('count') ||
+        dataclip.has_field?(/perc/)
       end
 
-      def call(dataclip, options = {})
+      def call(dataclip, args)
         Scrolls.log(step: :transform_dataclip, function: :base)
         @dataclip = dataclip
-        @options = options
+        @librato_base_name = args[:librato_base_name] || Config.default_librato_base_name
         gauges = dataclip.values.each_with_object({}) do |value, metrics|
-          metric_name = build_name(value)
-          metric_value = Float(value.count)
-          metrics[metric_name] = { value: metric_value }
+          if value.respond_to?(:count)
+            metric_name = build_name(value)
+            metric_value = Float(value.count)
+            metrics[metric_name] = { value: metric_value }
+          end
+          dataclip.matching_fields(/perc/).each do |field|
+            metric_name = build_name(value, field)
+            metric_value = Float(value.send(field))
+            metrics[metric_name] = { value: metric_value }
+          end
         end
         { gauges: gauges }
       end
 
       private
-      attr_reader :dataclip, :options
+      attr_reader :dataclip, :librato_base_name
 
       def build_name(value, metric_type = 'count')
-        base_name = if librato_base_name
-                      "#{librato_base_name}."
-                    else
-                      ""
-                    end
         if dataclip.has_field?(/name/)
-          "#{base_name}#{value.send(dataclip.matching_fields(/name/).first)}.#{metric_type}"
+          "#{librato_base_name}.#{value.send(dataclip.matching_fields(/name/).first)}.#{metric_type}"
         else
-          "#{base_name}#{metric_type}"
+          "#{librato_base_name}.#{metric_type}"
         end
-      end
-
-      def librato_base_name
-        options[:librato_base_name]
       end
     end
   end
