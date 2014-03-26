3567ce8b5a65cb8eef239492a2db201c906a1a49
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-26 12:37:31 +0100

    Add tests for librato client

---
+++ test/d2l/librato/client_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class D2L::Librato::ClientTest < Minitest::Unit::TestCase
+
+  def correct_metrics
+    {gauges: [{name: 'deploysaurus.users.count', value: 100}]}
+  end
+
+  def incorrect_metrics
+    {gauges: [{name: 'deploysaurus.users.count'}]}
+  end
+
+  def test_correct_metrics_are_submitted
+    VCR.use_cassette('librato_success') do
+      response = D2L::Librato::Client.new.submit(correct_metrics)
+      assert_equal 200, response.status
+    end
+  end
+
+  def test_correct_metrics_are_submitted
+    VCR.use_cassette('librato_failure') do
+      err = assert_raises D2L::Librato::Error do
+        response = D2L::Librato::Client.new.submit(incorrect_metrics)
+      end
+      assert_equal 400, err.status
+      assert_includes err.parsed_body, 'errors'
+    end
+  end
+end
