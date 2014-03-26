70418b9749bfff22dd119273c68c10fbe73918b3
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-26 16:59:21 +0100

    Add basic auth to sinatra

--- lib/d2l/web/app.rb
+++ lib/d2l/web/app.rb
@@ -13,6 +13,11 @@ require_relative 'routes'
 module D2L
   module Web
     class App < Sinatra::Application
+
+      use Rack::Auth::Basic, "Restricted Area" do |username, password|
+          username == D2L::Config.basic_auth_username and password == D2L::Config.basic_auth_password
+      end
+
       use Routes::Home
       use Routes::Measurements
     end
