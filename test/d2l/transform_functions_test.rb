28e23828b0417de7615d4a0de9e26855979edae1
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-27 16:27:01 +0100

    Test TransformFunctions find_for

---
+++ test/d2l/transform_functions_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+# TODO: test differently, fallback to default is hard to test when adding functions
+class D2L::TransformFunctionsTest < Minitest::Unit::TestCase
+  def test_find_global_counter
+    fields = ['some_value']
+    values = [Struct.new(:some_value).new(10)]
+    dataclip = D2L::Dataclips::Result.new(fields, values)
+    assert_kind_of D2L::TransformFunctions::GlobalCounters, D2L::TransformFunctions.find_for(dataclip)
+  end
+
+  def test_find_name_value
+    fields = ['some_value', 'another']
+    values = [Struct.new(:some_value).new(10)]
+    dataclip = D2L::Dataclips::Result.new(fields, values)
+    assert_kind_of D2L::TransformFunctions::NameValue, D2L::TransformFunctions.find_for(dataclip)
+  end
+
+  def test_fall_to_default
+    dataclip = D2L::Dataclips::Result.new([''], ['', '', ''])
+    assert_kind_of D2L::TransformFunctions::Default, D2L::TransformFunctions.find_for(dataclip)
+  end
+end
