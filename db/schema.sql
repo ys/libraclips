961409ae360f70bb89199e880142299df091f06f
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-28 11:29:03 +0100

    Add metrics table

--- db/schema.sql
+++ db/schema.sql
@@ -10,5 +10,14 @@ CREATE TABLE IF NOT EXISTS measurements (
   run_interval INTEGER
 );
 
+CREATE TABLE IF NOT EXISTS metrics (
+  id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  name TEXT NOT NULL,
+  duration FLOAT NOT NULL
+);
+
 CREATE INDEX measurements_run_at_run_interval ON measurements(run_at, run_interval);
 CREATE UNIQUE INDEX measurements_unique_dataclip_reference ON measurements(dataclip_reference);
+
+CREATE INDEX metrics_name ON metrics(name);
